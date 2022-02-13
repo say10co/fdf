1d1800072cb4f52764393a3471a0987b73ed0989
Ayoub Driouich <adriouic@e1r4p8.1337.ma>
2022-02-13 21:05:28 +0100

    none

--- c.sh
+++
@@ -1,2 +0,0 @@
-gcc -Wall -Werror -Wextra get_next_line/get_next_line.c get_next_line/get_next_line_utils.c  libft/libft.a parser.c drowonwindow.c key_handler.c interaction.c get_color.c -I /usr/X11/include -g -L /usr/X11/lib -l mlx -framework OpenGL -framework AppKit -o fdf
-
